7180056bbd1fc3419b02833ed75fa188c55f8425
Liam Storan <liam@DNa80dd49.SUNet>
2024-05-04 19:59:41 -0700

    added a distance calculation function

--- tsp/tsp_heuristic.py
+++ tsp/tsp_heuristic.py
@@ -21,16 +21,31 @@ def get_ref_reward(pointset):
         dist += ret_matrix[q[i], q[(i+1) % num_points]]
     return dist / CONST
 
+def get_distance_matrix(pointset):
+    num_points = len(pointset)
+    ret_matrix = np.zeros((num_points, num_points))
+    for i in range(num_points):
+        for j in range(i+1, num_points):
+            ret_matrix[i,j] = ret_matrix[j,i] = calc_dist(pointset[i], pointset[j])
+    return ret_matrix
 
-def nearest_neighbor(distance_matrix,sub_tour):
+
+# def get_ordered_sub_tour(sub_tour,placements):
+#     len_sub_tour = len(sub_tour)
+#     ordered_sub_tour = np.zeros(len_sub_tour,dtype = int)
+
+#     return ordered_sub_tour
+
+def nearest_neighbor(pointset,sub_tour,placements):
     """This function returns the next node from the nearest neighbor sequential heuristic.
-    Input: distance_matrix: 2D tensor whose i,j^th element is the distance on i -> j
-    sub_tour: list of selected city indices
+    Input: distance_matrix: 2D matrix whose i,j^th element is the distance on i -> j
+    sub_tour: list of selected city indices, placements is not used for this
     """
+    distance_matrix = get_distance_matrix(pointset)
     if len(sub_tour) == 0:
         return 0
     last_city = sub_tour[-1]
-    num_cities = distance_matrix.size()[0]
+    num_cities = np.shape(distance_matrix)[0]
     curr_min_dist = np.inf
     chosen_city_index = 0
     for i in range(num_cities):
@@ -44,14 +59,60 @@ def nearest_neighbor(distance_matrix,sub_tour):
 
     return chosen_city_index
     
+# def nearest(distance_matrix,sub_tour,placement):
+#     """Input: distance_matrix: 2D tensor, distance from i -> j is i,j^th element,
+#     sub_tour: list of city indices,
+#     placement: list of index of placement of each city"""
+
+#     if len(sub_tour) == 0:
+#         return 0
+#     num_cities = np.shape(distance_matrix)[0]
+#     #we find the closest city to the subtour
+#     min_dist = np.inf
+#     best_city = 0
+#     for i in range(num_cities):
+#         if i in sub_tour:
+#             continue
+#         local_min_dist = np.inf
+#         for j in sub_tour:
+#             curr_dist = distance_matrix[i,j]
+#             if curr_dist < local_min_dist:
+#                 local_min_dist = curr_dist
+#         if local_min_dist < min_dist:
+#             min_dist = local_min_dist
+#             best_city = i
+#     #now we find the optimal place to put the city
 
 
-#distance_matrix =torch.rand(5,5)
-#sub_tour = [0,2,3]
+    
+    
+            
+        
 
-distance_matrix = torch.zeros((5,5))+1
-distance_matrix[3,4] = 0
-distance_matrix[4,3] = 0
-sub_tour = [1,4]
-print(nearest_neighbor(distance_matrix,sub_tour))
 
+
+# def farthest(distance_matrix,sub_tour,placement):
+#     """Input: distance_matrix: 2D tensor, distance from i -> j is i,j^th element,
+#     sub_tour: list of city indices,
+#     placement: list of index of placement of each city"""
+#     if len(sub_tour) == 0:
+#         return 0
+#     num_cities = np.shape(distance_matrix)[0]
+#     #we find the farthest city to the subtour
+#     for i in range(num_cities):
+#         if i in sub_tour:
+#             continue
+#         max_dist = -np.inf
+#         best_city = 0
+#         for i in range(num_cities):
+#             if i in sub_tour:
+#                 continue
+#             local_max_dist = -np.inf
+#             for j in sub_tour:
+#                 curr_dist = distance_matrix[i,j]
+#                 if curr_dist > local_max_dist:
+#                     local_max_dist = curr_dist
+#             if local_max_dist > max_dist:
+#                 max_dist = local_max_dist
+#                 best_city = i
+print(get_distance_matrix(torch.tensor([[1,2],[3,4]])))
