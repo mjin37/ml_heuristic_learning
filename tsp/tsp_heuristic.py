aa0b889414267ee8c8509fb00827e90e4cca7d73
Liam Storan <liam@DNa80dd49.SUNet>
2024-05-04 19:15:04 -0700

    added nearest neighbor function

--- tsp/tsp_heuristic.py
+++ tsp/tsp_heuristic.py
@@ -20,3 +20,38 @@ def get_ref_reward(pointset):
     for i in range(num_points):
         dist += ret_matrix[q[i], q[(i+1) % num_points]]
     return dist / CONST
+
+
+def nearest_neighbor(distance_matrix,sub_tour):
+    """This function returns the next node from the nearest neighbor sequential heuristic.
+    Input: distance_matrix: 2D tensor whose i,j^th element is the distance on i -> j
+    sub_tour: list of selected city indices
+    """
+    if len(sub_tour) == 0:
+        return 0
+    last_city = sub_tour[-1]
+    num_cities = distance_matrix.size()[0]
+    curr_min_dist = np.inf
+    chosen_city_index = 0
+    for i in range(num_cities):
+        if i in sub_tour:
+            continue
+        else:
+            curr_dist = distance_matrix[last_city,i]
+            if curr_dist < curr_min_dist:
+                curr_min_dist = curr_dist
+                chosen_city_index = i
+
+    return chosen_city_index
+    
+
+
+#distance_matrix =torch.rand(5,5)
+#sub_tour = [0,2,3]
+
+distance_matrix = torch.zeros((5,5))+1
+distance_matrix[3,4] = 0
+distance_matrix[4,3] = 0
+sub_tour = [1,4]
+print(nearest_neighbor(distance_matrix,sub_tour))
+
