e67e238bd9c8aed3b871b515cdeab5f1ff1b7165
Matthew Jin <mjin73@stanford.edu>
2024-06-02 22:51:02 -0700

    Add force_prob sweeps

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import json
 import torch
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-print("PyTorch has version {}".format(torch.__version__))
-print('Using device:', device)
+# print("PyTorch has version {}".format(torch.__version__))
+# print('Using device:', device)
 
 from tsp.util import save, load, compute_solution
 from tsp.train import train
@@ -18,32 +18,35 @@ from tsp.model import RNNTSP
 import warnings
 warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
 
-num_runs = 3
-for i in range(num_runs):
-    class TSPArgs(argparse.Namespace):
-        def __init__(self):
-            self.model = RNNTSP
-            self.seq_len = 30
-            self.num_epochs = 200
-            self.num_tr_dataset = 10000
-            self.num_te_dataset = 2000
-            self.embedding_size = 128
-            self.hidden_size = 128
-            self.batch_size = 64
-            self.grad_clip = 1.5
-            self.use_cuda = True
-            self.beta = 0.9
-            self.force_prob = 0.8
-            self.heuristic = NearestNeighbor(device)
-            self.name = f"force_prob={self.force_prob}-{i}"
-            self.disable_tqdm = None
-    
-    
-    args = TSPArgs()
-    train_dataset = TSPDataset()
-    test_dataset = TSPDataset()
-    train_dataset.random_fill(args.seq_len, args.num_tr_dataset)
-    test_dataset.random_fill(args.seq_len, args.num_te_dataset)
-    
+class TSPArgs(argparse.Namespace):
+    def __init__(self):
+        self.model = RNNTSP
+        self.seq_len = 30
+        self.num_epochs = 200
+        self.num_tr_dataset = 10000
+        self.num_te_dataset = 2000
+        self.embedding_size = 128
+        self.hidden_size = 128
+        self.batch_size = 64
+        self.grad_clip = 1.5
+        self.use_cuda = True
+        self.beta = 0.9
+        self.force_prob = 1.0
+        self.heuristic = NearestNeighbor(device)
+        self.name = f"force_prob={self.force_prob}-4"
+        self.disable_tqdm = None
+
+args = TSPArgs()
+
+train_dataset = TSPDataset()
+test_dataset = TSPDataset()
+
+seed = torch.randint(100, (1,)).item()
+train_dataset.random_fill(args.seq_len, args.num_tr_dataset, random_seed=seed)
+test_dataset.random_fill(args.seq_len, args.num_te_dataset, random_seed=seed)
+
+for fp in [0.0, 0.2, 0.4, 0.6, 0.8]:
+    args.force_prob = fp
+    print(f"Force Probability = {args.force_prob}")
     model = train(train_dataset, test_dataset, args)
     save(model, args, args.name)
